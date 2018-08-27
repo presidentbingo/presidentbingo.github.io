b0250f2b56d8141f2edf77c2e97f8973d83e9855
presidentbingo <webuycats@gmail.com>
2018-08-27 17:38:48 -0400

    password_hidden

---
+++ password_protect.js
@@ -0,0 +1,21 @@
+function passWord() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Password',' ');
+    while (testV < 5) {
+    if (!pass1) 
+    history.go(-1);
+    if (pass1.toLowerCase() == "lol") {
+    alert('Welcome.');
+    window.open('http://www.bingnut.me/school');
+    break;
+    } 
+    testV+=1;
+    var pass1 = 
+    prompt('Access Denied - Password Incorrect, Please Try Again.','');
+    }
+    if (pass1.toLowerCase()!="password" & testV == 5) 
+    history.go(-1);
+    return " ";
+    } 
+
+passWord();
